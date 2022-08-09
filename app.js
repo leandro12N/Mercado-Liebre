d01ce4f516339a40528405ee5efc49e3b7591e09
unknown <leandromanuel291295@gmail.com>
2022-08-09 20:29:15 -0300

    actualizando de nuevo

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const publicPath = path.join(__dirname,"./public")
 
 app.use(express.static(publicPath))
 
+app.get("/", function(req,res){
+    res.sendFile(path.join(__dirname,"views/home.html"))
+})
 app.listen(3000, () => {
     console.log ("servidor 3000 corriendo")
 });
