3d03e154ebcd14303246418e2c560e40d502a28b
unknown <leandromanuel291295@gmail.com>
2022-08-23 15:49:35 -0300

    agregando login y registro en html y css

--- app.js
+++ app.js
@@ -7,7 +7,13 @@ const publicPath = path.join(__dirname,"./public")
 app.use(express.static(publicPath))
 
 app.get("/", function(req,res){
-    res.sendFile(path.join(__dirname,"views/home.html"))
+    res.sendFile(path.join(__dirname,"/views/home.html"))
+})
+app.get("/registro", function(req,res){
+    res.sendFile(path.join(__dirname,"/views/registro.html"))
+})
+app.get("/login", function(req,res){
+    res.sendFile(path.join(__dirname,"/views/login.html"))
 })
 app.listen(3000, () => {
     console.log ("servidor 3000 corriendo")
